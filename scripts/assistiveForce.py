205828163bccb7ba95656ac9058087dca2855fea
Xabier Irizar <irizar.xabier@gmail.com>
2023-11-15 17:11:39 -0500

    Refactoring of opensim simulation script

--- scripts/assistiveForce.py
+++ scripts/assistiveForce.py
@@ -9,105 +9,78 @@ from pathlib import Path
 from assistive_arm.moco_helpers import (
     get_model,
     get_tracking_problem,
+    set_moco_problem_weights,
 )
 from assistive_arm.utils.file_modification import modify_force_xml
 
 
 def main():
-    enable_assist = False
-
     # Dict for yaml file
     config_file = dict()
 
     subject = "subject_4"
-    subject_data = Path("/Users/xabieririzar/Desktop/Life/Studium/TUM/M.Sc Robotics/Masterarbeit Harvard/Thesis/Subject testing/Subject data/") / subject
-    trial = subject_data / "trial_4"
-
-    model_name = f"{subject}_full"  # simple / full
+    subject_data = (
+        Path(
+            "/Users/xabieririzar/Desktop/Life/Studium/TUM/M.Sc Robotics/Harvard/Thesis/Subject testing/Subject data/"
+        )
+        / subject
+    )
+    model_name = f"{subject}_simple"  # simple / full
     model_path = subject_data / "model" / "LaiUhlrich2022_scaled.osim"
+
+    trial = subject_data / "trial_3"
 
     config_file["subject"] = subject
     config_file["trial"] = str(trial)
+    config_file["reserve_pelvis_weight"] = 15
+
+    assistive_force = None  # 700N or None
 
     t_0 = 5.7
-    t_f = 7.7
+    t_f = 7.25
     mesh_interval = 0.05
-    
+
     config_file["t_0"] = t_0
     config_file["t_f"] = t_f
     config_file["mesh_interval"] = mesh_interval
-
-    modify_force_xml('./moco/forces/grf_sit_stand.xml', str(trial / "grf_filtered.mot"))
-
     config_file["grf_path"] = str(trial / "grf_filtered.mot")
 
+    modify_force_xml("./moco/forces/grf_sit_stand.xml", str(trial / "grf_filtered.mot"))
 
     model = get_model(
         subject_name=model_name,
         model_path=model_path,
         target_path=model_path.parent,
-        enable_assist=enable_assist,
+        assistive_force=assistive_force,
         ground_forces=True,
+        minimal_actuators=False,
         config=config_file,
     )
     model.initSystem()
 
-
     tracking_problem = get_tracking_problem(
-        model=model, markers_path=str(trial / "opencap_tracker.trc"), t_0=t_0, t_f=t_f, mesh_interval=mesh_interval
+        model=model,
+        markers_path=str(trial / "opencap_tracker.trc"),
+        t_0=t_0,
+        t_f=t_f,
+        mesh_interval=mesh_interval,
     )
     study = tracking_problem.initialize()
 
-    problem = study.updProblem()
-    problem.addGoal(osim.MocoInitialActivationGoal("activation"))
-
-    effort_goal = osim.MocoControlGoal.safeDownCast(problem.updGoal("control_effort"))
-
-    # Increase weight of pelvis coordinate
-    pelvis_weight = 15
-    config_file["reserve_pelvis_weight"] = pelvis_weight
-
-    # TODO Should  be packed in a function
-    effort_goal.setWeightForControl("/reserve_pelvis_tilt", pelvis_weight)
-    effort_goal.setWeightForControl("/reserve_pelvis_rotation", pelvis_weight)
-    effort_goal.setWeightForControl("/reserve_pelvis_list", pelvis_weight)
-    effort_goal.setWeightForControl("/reserve_pelvis_tx", pelvis_weight)
-    effort_goal.setWeightForControl("/reserve_pelvis_ty", pelvis_weight)
-    effort_goal.setWeightForControl("/reserve_pelvis_tz", pelvis_weight)
-
-    forceSet = model.getForceSet()  
-    for i in range(forceSet.getSize()):
-        forcePath = forceSet.get(i).getAbsolutePathString()
-        if 'pelvis' in str(forcePath):
-            print("Adjusting residual force weight")
-            effort_goal.setWeightForControl(forcePath, pelvis_weight)
-
-    # Set weights for muscles
-    effort_goal.setWeightForControl("/forceset/recfem_r", 1)
-    effort_goal.setWeightForControl("/forceset/vasmed_r", 1)
-    effort_goal.setWeightForControl("/forceset/recfem_l", 1)
-    effort_goal.setWeightForControl("/forceset/vasmed_l", 1)
-    effort_goal.setWeightForControl("/forceset/soleus_r", 1)
-    effort_goal.setWeightForControl("/forceset/soleus_l", 1)
-    effort_goal.setWeightForControl("/forceset/tibant_r", 1)
-    effort_goal.setWeightForControl("/forceset/tibant_l", 1)
-
-
-    if enable_assist:
-        effort_goal.setWeightForControl("/forceset/assistive_force_y", 0)
-        effort_goal.setWeightForControl("/forceset/assistive_force_x", 0)
+    set_moco_problem_weights(model=model, moco_study=study, config=config_file)
 
     cur_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    solution_name = f"{model_name}_{trial.stem}_assistance_{str(enable_assist).lower()}_{cur_time}"
+    solution_name = f"{model_name}_{trial.stem}_assistance_{str(assistive_force).lower()}_{cur_time}"
 
     config_file["solution_name"] = solution_name
-    config_file["solution_path"] = f"./moco/control_solutions/{solution_name}.sto"
+    config_file["solution_path"] = str(trial / f"/control_solutions/{solution_name}.sto")
 
     with open(f"./moco/control_solutions/{solution_name}.yaml", "w") as f:
         yaml.dump(config_file, f)
 
     solution = study.solve()
     solution.write(f"./moco/control_solutions/{solution_name}.sto")
+    solution.write(str(trial / f"{solution_name}.sto"))
     study.visualize(solution)
 
 
