c96676464fc1b600129ec4f825413251cbda8a89
teoamigoni <mamigoni@g.harvard.edu>
2025-04-08 12:25:39 -0400

    input for SVR choice on fast computation

--- Phase Estimation/main.py
+++ Phase Estimation/main.py
@@ -51,23 +51,7 @@ try:
     models = {'ridge': None, 'lasso': None, 'linear': None}
     results = {'ridge': None, 'lasso': None, 'linear': None}
     
-    # parameter grid for SVR, reduce the number of parameters for faster computation
-    # Note: The grid search will take longer with more parameters
-    fast_comput = True
-    if fast_comput:
-        # Reduced grid (3x3x3 = 27 combinations)
-        param_grid = {
-            'svr__C': np.logspace(1, 3, 3),       # [10, 100, 1000]
-            'svr__epsilon': np.logspace(-3, -1, 3), # [0.001, 0.01, 0.1]
-            'svr__gamma': np.logspace(-3, -1, 3)   # [0.001, 0.01, 0.1]
-        }   
-    else:
-        # Enhanced grid (7x4x6 = 168 combinations)
-        param_grid = {
-            'svr__C': np.logspace(-3, 3, 7),
-            'svr__epsilon': np.logspace(-3, 0, 4),
-            'svr__gamma': np.logspace(-4, 1, 6)
-            }
+    
     
     # Perform selected regression(s)
     if choice == '1':
@@ -77,12 +61,45 @@ try:
     elif choice == '3':
         linear_model, y_linear, _, mse_linear = Linear_Reg.linear_regression(X,Y, frequencies, feature_names=feature_names,  plot=True)
     elif choice == '5':
+        # parameter grid for SVR, reduce the number of parameters for faster computation
+        # Note: The grid search will take longer with more parameters
+        fast_comput = input("Do you want to reduce the number of parameters for faster SVR computation? (yes/no): ").strip().lower()
+        if fast_comput == 'yes':
+            # Reduced grid (3x3x3 = 27 combinations)
+            param_grid = {
+                'svr__C': np.logspace(1, 3, 3),       # [10, 100, 1000]
+                'svr__epsilon': np.logspace(-3, -1, 3), # [0.001, 0.01, 0.1]
+                'svr__gamma': np.logspace(-3, -1, 3)   # [0.001, 0.01, 0.1]
+            }   
+        else:
+            # Enhanced grid (7x4x6 = 168 combinations)
+            param_grid = {
+                'svr__C': np.logspace(-3, 3, 7),
+                'svr__epsilon': np.logspace(-3, 0, 4),
+                'svr__gamma': np.logspace(-4, 1, 6)
+                }
         ridge_result, y_ridge = RidgeRegressionCV.enhanced_ridge_regression(X,Y,feature_names,alpha_range=(-7, 7, 40), cv=None, plot=True, frequencies=frequencies)
         lasso_result, y_lasso = LassoRegressionCV.enhanced_lasso_regression(X,Y,feature_names,alpha_range=(-7, 7, 40), cv=None, plot=True, frequencies=frequencies)
         linear_model, y_linear, _, mse_linear = Linear_Reg.linear_regression(X,Y, frequencies, feature_names=feature_names, plot=True)
-        # svr_model, y_svr = LassoRegressionCV.enhanced_lasso_regression(X,Y,feature_names,alpha_range=(-7, 7, 40), cv=None, plot=True, frequencies=frequencies)
         svr_model, y_svr = SVR_Reg.enhanced_svr_regression(X,Y, kernel='rbf', param_grid=param_grid, plot=True, frequencies=frequencies)
     elif choice == '4':
+        # parameter grid for SVR, reduce the number of parameters for faster computation
+        # Note: The grid search will take longer with more parameters
+        fast_comput = input("Do you want to reduce the number of parameters for faster SVR computation? (yes/no): ").strip().lower()
+        if fast_comput == 'yes':
+            # Reduced grid (3x3x3 = 27 combinations)
+            param_grid = {
+                'svr__C': np.logspace(1, 3, 3),       # [10, 100, 1000]
+                'svr__epsilon': np.logspace(-3, -1, 3), # [0.001, 0.01, 0.1]
+                'svr__gamma': np.logspace(-3, -1, 3)   # [0.001, 0.01, 0.1]
+            }   
+        else:
+            # Enhanced grid (7x4x6 = 168 combinations)
+            param_grid = {
+                'svr__C': np.logspace(-3, 3, 7),
+                'svr__epsilon': np.logspace(-3, 0, 4),
+                'svr__gamma': np.logspace(-4, 1, 6)
+                }
         svr_model, y_svr = SVR_Reg.enhanced_svr_regression(X,Y, kernel='rbf', param_grid=param_grid, plot=True, frequencies=frequencies)
     else:
         print("Invalid choice")
